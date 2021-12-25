3a845488156115b5bf357213b5b237b31c160702
Wassim Mansouri <wassim.mansouri@gmail.com>
2021-12-25 20:53:23 +0100

    WeSplit initial commit
    Create WeSplit's new project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Wassim Mansouri on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
