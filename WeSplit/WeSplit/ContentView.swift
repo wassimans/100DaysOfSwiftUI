3cfa0e5eb2325aa1206abb9294e678dabab8052e
Wassim Mansouri <wassim.mansouri@gmail.com>
2022-01-02 22:03:44 +0100

    Update WeSplit
    Make total amount of colour red if the user selects 0% tip.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -63,6 +63,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(totalAmount, format: currencyFormatter)
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
                 } header: {
                     Text("Total amount")
                 }
