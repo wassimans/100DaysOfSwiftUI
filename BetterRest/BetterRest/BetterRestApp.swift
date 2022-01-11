a6b5679ab92d91b4d674db7a75fd6fe6ee16da49
Wassim Mansouri <wassim.mansouri@gmail.com>
2022-01-11 06:25:26 +0100

    GuessTheFlag basic project structure

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Wassim Mansouri on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
